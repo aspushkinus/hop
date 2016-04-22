bad0fd378a4becde867a95d85f3d9d4f12976f38
Sebastian <sebasporto@gmail.com>
2016-04-22 17:09:12 +1000

    Attempt to run rust in docker

--- test/integration/examples/both/run.sh
+++ test/integration/examples/both/run.sh
@@ -2,16 +2,19 @@
 
 mix deps.get
 
-# Wait for Elm
-until nc -z $APP_HOST $APP_PORT; do
-  echo "Waiting for $APP_HOST ..."
-  sleep 1
-done
+cd ping && cargo run $APP_HOST:$APP_PORT
+cd ping && cargo run $WEBDRIVER_HOST:$WEBDRIVER_PORT
 
-# Wait for Webdriver
-until nc -z $WEBDRIVER_HOST $WEBDRIVER_PORT; do
-  echo "Waiting for $WEBDRIVER_HOST ..."
-  sleep 1
-done
+# # Wait for Elm
+# until nc -z $APP_HOST $APP_PORT; do
+#   echo "Waiting for $APP_HOST ..."
+#   sleep 1
+# done
+
+# # Wait for Webdriver
+# until nc -z $WEBDRIVER_HOST $WEBDRIVER_PORT; do
+#   echo "Waiting for $WEBDRIVER_HOST ..."
+#   sleep 1
+# done
 
 mix test test/$TEST_DIR
