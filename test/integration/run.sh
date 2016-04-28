e423ed1ce38a69c0ad2d904cdb1108970345fc15
Sebastian Porto <sebasporto@gmail.com>
2016-04-28 12:47:50 +1000

    Revert to standalone-chrome

--- test/integration/run.sh
+++ test/integration/run.sh
@@ -6,6 +6,7 @@ until nc -z $APP_HOST $APP_PORT; do
   echo "Waiting for $APP_HOST ..."
   sleep 1
 done
+echo "Found $APP_HOST"
 
 # Wait for Webdriver
 echo "Looking for webdriver at $WEBDRIVER_HOST"
@@ -13,5 +14,6 @@ until nc -z $WEBDRIVER_HOST $WEBDRIVER_PORT; do
   echo "Waiting for $WEBDRIVER_HOST ..."
   sleep 1
 done
+echo "Found $WEBDRIVER_HOST"
 
 ginkgo
